3be6bdd6e69451db6c04de3a49066617d1e45b1f
Joe Terlecki <joe.p.terlecki@gmail.com>
2024-12-24 00:26:13 -0500

    more conditional checks

---
+++ dev/docker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/../utils/log.sh"
+
+log_section "Docker"
+
+if command -v docker &> /dev/null; then
+    log_warn "Docker installation found..."
+    log_warn "Skipping installation"
+else
+
+    log_info "Removing docker installations if present..."
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+    log_info "Retrieving convienience script..."
+    curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+
+    log_info "Executing Docker installation script..."
+    sudo sh /tmp/get-docker.sh
+
+    log_info "Enabling and starting Docker..."
+    sudo systemctl enable docker && sudo systemctl start docker
+fi
+
+log_info "Docker installation complete"
--- dev/nvm.sh
+++ dev/nvm.sh
@@ -4,13 +4,43 @@ source "$(dirname "$0")/../utils/log.sh"
 
 log_section "Node Version Manager"
 
-log_info "Installing node version manager (NVM)..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+log_info "Calculating latest Node Version Manager version..."
+LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+INSTALL_URL="https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh"
 
-log_info "Sourcing NVM..."
-source ~/.nvm/nvm.sh
+log_warn "Setting NVM_DIR to check previous installation..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 
-log_info "Installing curretn Node LTS..."
-nvm install --lts
+if command -v nvm &> /dev/null; then
+    CURRENT_VERSION="v$(nvm --version)"
+    log_info "Current version: ${CURRENT_VERSION}"
+    log_info "Latest version: ${LATEST_VERSION}"
+    
+    if [ "${CURRENT_VERSION}" = "${LATEST_VERSION}" ]; then
+        log_warn "Already running the latest version of Node Version Manager"
+        log_info "Node Version Manager and Node LTS installation completed"
+        exit 0
+    fi
+    
+    log_warn "Previous Node Version Manager (NVM) installation found"
+    log_warn "Executing default behaviour for updates..."
+    curl -o- "$INSTALL_URL" | bash
+
+    log_info "Sourcing NVM..."
+    source ~/.nvm/nvm.sh
+
+    log_info "Installing current Node LTS..."
+    nvm install --lts
+else
+    log_info "Installing node version manager (NVM)..."
+    curl -o- "$INSTALL_URL" | bash
+
+    log_info "Sourcing NVM..."
+    source ~/.nvm/nvm.sh
+
+    log_info "Installing current Node LTS..."
+    nvm install --lts
+fi
 
 log_info "Node Version Manager and Node LTS installation completed"
