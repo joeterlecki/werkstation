ccce539fb36797fd84808679a3d3734b4913e6ed
Joe Terlecki <joe.p.terlecki@gmail.com>
2025-01-04 00:46:04 -0500

    chaning my mind about fedora

--- base/essential.sh
+++ base/essential.sh
@@ -4,18 +4,18 @@ source "$(dirname "$0")/../utils/log.sh"
 log_section "Essential Packages"
 
 PACKAGES=(
-    curl
-    wget
-    git
-    unzip
-    build-essential
-    software-properties-common
-    apt-transport-https
-    ca-certificates
-    gnupg
-    jq
-    fuse
-    stow
+	curl
+	wget
+	git
+	unzip
+	build-essential
+	software-properties-common
+	apt-transport-https
+	ca-certificates
+	gnupg
+	jq
+	fuse
+	stow
 )
 
 log_info "Installing common tools..."
--- install.sh
+++ install.sh
@@ -5,87 +5,86 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/utils/log.sh"
 
 check_environment() {
-    if ! command -v apt-get >/dev/null; then
-        log_error "This script only supports Fedora based systems"
-        exit 1
-    fi
+	if ! command -v apt-get >/dev/null; then
+		log_error "This script only supports Fedora based systems"
+		exit 1
+	fi
 
-    if grep -qi microsoft /proc/version; then
-        export IS_WSL=1
-        log_info "WSL environment detected"
-    else
-        export IS_WSL=0
-        log_info "Native Linux environment detected"
-    fi
+	if grep -qi microsoft /proc/version; then
+		export IS_WSL=1
+		log_info "WSL environment detected"
+	else
+		export IS_WSL=0
+		log_info "Native Linux environment detected"
+	fi
 
-    arch=$(uname -m)
-    case $arch in
-    x86_64)
-        export ARCH="amd64"
-        ;;
-    aarch64 | arm64)
-        export ARCH="arm64"
-        ;;
-    *)
-        log_error "Architecture not supported: $arch"
-        exit 1
-        ;;
-    esac
-    log_info "Detected architecture: $ARCH"
+	arch=$(uname -m)
+	case $arch in
+	x86_64)
+		export ARCH="amd64"
+		;;
+	aarch64 | arm64)
+		export ARCH="arm64"
+		;;
+	*)
+		log_error "Architecture not supported: $arch"
+		exit 1
+		;;
+	esac
+	log_info "Detected architecture: $ARCH"
 }
 
-# TODO CONVERT TO FEDORA
 update_system() {
-    log_section "Running system updates"
-    log_info "Updating package lists..."
-    sudo apt-get update
+	log_section "Running system updates"
+	log_info "Updating package lists..."
+	sudo apt-get update
 
-    log_info "Upgrading packages..."
-    sudo apt-get upgrade -y
+	log_info "Upgrading packages..."
+	sudo apt-get upgrade -y
 
-    log_info "Cleaning up apt caches..."
-    sudo apt-get autoremove -y
-    sudo apt-get autoclean
+	log_info "Cleaning up apt caches..."
+	sudo apt-get autoremove -y
+	sudo apt-get autoclean
 }
 
 execute() {
-    local dir=$1
-    if [[ ! -d "$dir" ]]; then
-        log_error "Directory: $dir not found"
-        return 1
-    fi
+	local dir=$1
+	if [[ ! -d "$dir" ]]; then
+		log_error "Directory: $dir not found"
+		return 1
+	fi
 
-    log_section "Processing directory: $SCRIPT_DIR/$(basename "$dir")"
+	log_section "Processing directory: $SCRIPT_DIR/$(basename "$dir")"
 
-    while IFS= read -r script; do
-        if [[ -f "$script" && "$script" != "${SCRIPT_DIR}/install.sh" ]]; then
-            if ! bash "$script"; then
-                log_error "Failed to execute script: $script"
-                return 1
-            fi
-        fi
-    done < <(find "$dir" -type f -name "*.sh" | sort)
+	while IFS= read -r script; do
+		if [[ -f "$script" && "$script" != "${SCRIPT_DIR}/install.sh" ]]; then
+			if ! bash "$script"; then
+				log_error "Failed to execute script: $script"
+				return 1
+			fi
+		fi
+	done < <(find "$dir" -type f -name "*.sh" | sort)
 }
 
 main() {
-    check_environment
+	check_environment
 
-    log_warn "Retrieving SUDO password for elevated actions"
-    sudo -v
+	log_warn "Retrieving SUDO password for elevated actions"
+	sudo -v
 
-    update_system
+	update_system
 
-    export IS_WSL
-    export ARCH
-    export SCRIPT_DIR
-    export -f log_info log_warn log_error log_section
-    export RED GREEN YELLOW PURPLE NC
+	export IS_WSL
+	export ARCH
+	export SCRIPT_DIR
+	export -f log_info log_warn log_error log_section
+	export RED GREEN YELLOW PURPLE NC
 
-    log_section "Starting Installation"
-    execute "$SCRIPT_DIR/base"
-    execute "$SCRIPT_DIR/dev"
-    execute "$SCRIPT_DIR/neovim"
-    log_section "Installation script complete"
+	log_section "Starting Installation"
+	execute "$SCRIPT_DIR/base"
+	execute "$SCRIPT_DIR/dev"
+	execute "$SCRIPT_DIR/neovim"
+	log_section "Installation script complete"
 }
 
 main
