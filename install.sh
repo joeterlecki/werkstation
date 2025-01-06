a4e778fb6b0be61787315ee4ffcf3daae15dada9
Joe Terlecki <joe.p.terlecki@gmail.com>
2025-01-06 17:56:45 -0500

    comment

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ main() {
 	log_section "Starting Installation"
 	execute "$SCRIPT_DIR/base"
 	execute "$SCRIPT_DIR/dev"
-	# execute "$SCRIPT_DIR/neovim"
+	execute "$SCRIPT_DIR/neovim"
 	log_section "Installation script complete"
 }
 
