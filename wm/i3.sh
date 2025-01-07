d2c14499470311b539abd1c3419f453654ef8d37
Joe Terlecki <joe.p.terlecki@gmail.com>
2025-01-07 00:52:10 -0500

    thunar

--- wm/i3.sh
+++ wm/i3.sh
@@ -3,6 +3,6 @@ source "${SCRIPT_DIR}/utils/log.sh"
 log_section "Window Manager"
 
 log_info "Installing i3 window manager and required packages..."
-sudo dnf install i3 picom feh polybar dunst lxappearance flameshot -y
+sudo dnf install i3 picom feh polybar dunst lxappearance flameshot thunar -y
 
 log_info "Window manager installation complete"
