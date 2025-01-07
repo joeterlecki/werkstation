e8f47a18ce02c5e444e1ac0cf0a29107a67973e7
Joe Terlecki <joe.p.terlecki@gmail.com>
2025-01-06 23:47:28 -0500

    removing polybar

--- wm/i3.sh
+++ wm/i3.sh
@@ -3,6 +3,6 @@ source "${SCRIPT_DIR}/utils/log.sh"
 log_section "Window Manager"
 
 log_info "Installing i3 window manager and required packages..."
-sudo dnf install i3 picom feh polybar dunst -y
+sudo dnf install i3 picom feh dunst -y
 
 log_info "Window manager installation complete"
