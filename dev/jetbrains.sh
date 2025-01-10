83b4223c9f036018b4c85b326295703323d5a057
Joe Terlecki <joeterlecki@proton.me>
2025-01-10 14:23:36 -0500

    formatting

--- dev/jetbrains.sh
+++ dev/jetbrains.sh
@@ -8,39 +8,39 @@ JETBRAINS_PATH='export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts"
 WAYLAND_RUNTIME='ln -sf /mnt/wslg/runtime-dir/wayland-* $XDG_RUNTIME_DIR/'
 
 if [[ ":$PATH:" != *":$HOME/.local/share/JetBrains/Toolbox/scripts:"* ]]; then
-   log_warn "Toolbox scripts not found in current PATH"
+	log_warn "Toolbox scripts not found in current PATH"
 else
-   log_info "Toolbox scripts found in current PATH"
+	log_info "Toolbox scripts found in current PATH"
 fi
 
 if ! grep -q "PATH=\"\$PATH:\$HOME/.local/share/JetBrains/Toolbox/scripts\"" "$HOME/.bashrc"; then
-   log_info "Adding Toolbox scripts PATH to .bashrc..."
-   echo "$JETBRAINS_PATH" >> "$HOME/.bashrc"
+	log_info "Adding Toolbox scripts PATH to .bashrc..."
+	echo "$JETBRAINS_PATH" >>"$HOME/.bashrc"
 else
-   log_info "Toolbox scripts PATH already in .bashrc"
+	log_info "Toolbox scripts PATH already in .bashrc"
 fi
 
 if ! grep -q "PATH=\"\$PATH:\$HOME/.local/share/JetBrains/Toolbox/scripts\"" "$HOME/.zshrc"; then
-   log_info "Adding Toolbox scripts PATH to .zshrc..."
-   echo "$JETBRAINS_PATH" >> "$HOME/.zshrc"
+	log_info "Adding Toolbox scripts PATH to .zshrc..."
+	echo "$JETBRAINS_PATH" >>"$HOME/.zshrc"
 else
-   log_info "Toolbox scripts PATH already in .zshrc"
+	log_info "Toolbox scripts PATH already in .zshrc"
 fi
 
 if [[ $IS_WSL -eq 1 ]]; then
-   if ! grep -q "ln -sf /mnt/wslg/runtime-dir/wayland-\* \$XDG_RUNTIME_DIR" "$HOME/.bashrc"; then
-       log_info "Adding Wayland runtime configuration to .bashrc..."
-       echo "$WAYLAND_RUNTIME" >> "$HOME/.bashrc"
-   fi
-   if ! grep -q "ln -sf /mnt/wslg/runtime-dir/wayland-\* \$XDG_RUNTIME_DIR" "$HOME/.zshrc"; then
-       log_info "Adding Wayland runtime configuration to .zshrc..."
-       echo "$WAYLAND_RUNTIME" >> "$HOME/.zshrc"
-   fi
+	if ! grep -q "ln -sf /mnt/wslg/runtime-dir/wayland-\* \$XDG_RUNTIME_DIR" "$HOME/.bashrc"; then
+		log_info "Adding Wayland runtime configuration to .bashrc..."
+		echo "$WAYLAND_RUNTIME" >>"$HOME/.bashrc"
+	fi
+	if ! grep -q "ln -sf /mnt/wslg/runtime-dir/wayland-\* \$XDG_RUNTIME_DIR" "$HOME/.zshrc"; then
+		log_info "Adding Wayland runtime configuration to .zshrc..."
+		echo "$WAYLAND_RUNTIME" >>"$HOME/.zshrc"
+	fi
 fi
 
 if [ -L "$BINARY_LINK" ] || [ -d "$TOOLBOX_DIR" ] || pgrep -f "jetbrains-toolbox" &>/dev/null; then
-   log_warn "Jetbrains Toolbox previous installation exists..."
-   exit 0
+	log_warn "Jetbrains Toolbox previous installation exists..."
+	exit 0
 fi
 
 TEMP_DIR=$(mktemp -d)
@@ -48,23 +48,23 @@ TEMP_DIR=$(mktemp -d)
 log_info "Detecting latest version..."
 LATEST_URL=$(wget -qO- "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release" | jq -r '.TBA[0].downloads.linux.link')
 if [ -z "$LATEST_URL" ] || [ "$LATEST_URL" == "null" ]; then
-   log_error "Failed to retrieve latest version from url"
-   exit 1
+	log_error "Failed to retrieve latest version from url"
+	exit 1
 fi
 
 log_info "Retrieving latest Jetbrains Toolbox version..."
 wget -O "$TEMP_DIR/toolbox.tar.gz" "$LATEST_URL"
 if [ ! -f "$TEMP_DIR/toolbox.tar.gz" ]; then
-   log_error "Failed to retrieve toolbox archive"
-   exit 1
+	log_error "Failed to retrieve toolbox archive"
+	exit 1
 fi
 
 log_info "Extracting Jetbrains Toolbox archive..."
 tar -xzf "$TEMP_DIR/toolbox.tar.gz" -C "$TEMP_DIR"
 EXTRACT_DIR=$(find "$TEMP_DIR" -maxdepth 1 -type d -name "jetbrains-toolbox-*")
 if [ -z "$EXTRACT_DIR" ]; then
-   log_error "Extraction failed"
-   exit 1
+	log_error "Extraction failed"
+	exit 1
 fi
 
 log_info "Installing Toolbox to $TOOLBOX_DIR..."
