11abc41ba2dcaa9d4744d7428965cd0e86fe56e4
Joe Terlecki <joeterlecki@proton.me>
2025-01-18 11:12:33 -0500

    refactor

---
+++ dotfiles/dot_config/nvim/lua/plugins/astroui.lua
@@ -0,0 +1,10 @@
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    colorscheme = "catppuccin-mocha",
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    },
+    -- Icons can be configured throughout the interface
+}
