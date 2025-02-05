8decf085107efaced0027e168d7c0b386a61eb13
Joe Terlecki <jterlecki@localhost-live.localdomain>
2025-02-05 14:54:00 -0500

    rebuild

--- src/apps/kitty.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source "${SCRIPT_DIR}/utils/log.sh"
-log_section "Kitty Terminal"
-
-log_info "Installing kitty terminal..."
-sudo dnf install kitty -y
-
-log_info "Kitty terminal installation complete"
-
