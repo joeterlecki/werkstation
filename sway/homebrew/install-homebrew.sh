c5d1963d2e858ecaa9a764080123d138ae8d8d12
Joe Terlecki <joe.p.terlecki@gmail.com>
2025-12-04 12:37:16 -0500

    brew perms

--- sway/homebrew/install-homebrew.sh
+++ sway/homebrew/install-homebrew.sh
@@ -2,6 +2,7 @@
 
 BREW_PREFIX="/home/linuxbrew/.linuxbrew"
 
+# Check if brew is already installed
 if [ -x "${BREW_PREFIX}/bin/brew" ]; then
     echo "Homebrew already installed at ${BREW_PREFIX}"
     exit 0
@@ -9,13 +10,17 @@ fi
 
 echo "Installing Homebrew..."
 
+# Create directory structure
 mkdir -p /home/linuxbrew/.linuxbrew
 
+# Clone Homebrew
 git clone https://github.com/Homebrew/brew "${BREW_PREFIX}/Homebrew"
 
+# Create bin directory and symlink
 mkdir -p "${BREW_PREFIX}/bin"
 ln -s "${BREW_PREFIX}/Homebrew/bin/brew" "${BREW_PREFIX}/bin/brew"
 
-chmod -R 755 /home/linuxbrew
+# Make it world-writable so any user can use it
+chmod -R 777 /home/linuxbrew
 
 echo "Homebrew installed successfully"
